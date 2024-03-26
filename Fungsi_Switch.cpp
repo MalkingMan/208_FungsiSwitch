f71cd7d8aacd5269f7b4d9f5d151cb69375d6575
MalkingMan <arrayalkhozini@gmail.com>
2024-03-26 15:11:31 +0700

    Case 4

--- Fungsi_Switch.cpp
+++ Fungsi_Switch.cpp
@@ -63,7 +63,10 @@ int main()
             inputData();
             cout << "Hasil perkaliam =" << perkalian(bilangan1, bilangan2) << endl;
         break;
-        
+        case 4: 
+            inputData();
+            cout << "Hasil pembagian =" << pembagian(bilangan1, bilangan2) << endl;
+        break;
     
     default:
         break;
