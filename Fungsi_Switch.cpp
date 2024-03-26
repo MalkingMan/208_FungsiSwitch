50b0b37f7f111b0d4c18e2ddeaa62feb6e3d1484
MalkingMan <arrayalkhozini@gmail.com>
2024-03-26 14:28:43 +0700

    Input data

---
+++ Fungsi_Switch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+void inputData()
+{
+    cout << "Masukkan bilangan pertama :"
+    cin >> bilangan1;
+    cout << "Masukkan bilangan kedua :"
+    cin >> bilangan2;
+}
