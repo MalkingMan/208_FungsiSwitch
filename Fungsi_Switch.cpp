7734f1f010ca3ff547c16b05d4495e40afca23bc
MalkingMan <arrayalkhozini@gmail.com>
2024-03-26 14:41:30 +0700

    pengurangan

--- Fungsi_Switch.cpp
+++ Fungsi_Switch.cpp
@@ -5,8 +5,17 @@ int bilangan1, bilangan2;
 
 void inputData()
 {
-    cout << "Masukkan bilangan pertama :"
+    cout << "Masukkan bilangan pertama :";
     cin >> bilangan1;
-    cout << "Masukkan bilangan kedua :"
+    cout << "Masukkan bilangan kedua :";
     cin >> bilangan2;
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+int pengurangan(int a, int b)
+{
+    return a + b;
 }
